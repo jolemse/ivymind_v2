e9d97a2ab3ba4335e422a802d805f1a44f954d17
jolemse <35252911+jolemse@users.noreply.github.com>
2023-12-11 19:41:20 +0100

    Ranke deutlich weiter

--- app/Startseite/RankeVorteile.tsx
+++ app/Startseite/RankeVorteile.tsx
@@ -1,21 +1,67 @@
 'use client';
 
-import { Center, Container, Image } from '@mantine/core';
+import { Container, Image, Grid, BackgroundImage, Box, AspectRatio } from '@mantine/core';
+
 import classes from './Blickfang.module.css';
 
 export default function RankeVorteile() {
   return (
     <div className={classes.solidBackgroundRanke}>
       <Container>
-        <Center>
-          <Image src="/img/Startseite/Ivymind_Ranke.svg" w="60%" style={{ transform: 'translateY(-1rem)' }} />
-        </Center>
-        <ul>
-          <li>Finden Statt Suchen</li>
-          <li>Kostenloses Erstgespräch</li>
-          <li>Kein Risiko</li>
-          <li>Qualifizierte Anbieter:innen</li>
-        </ul>
+        <Grid style={{ overflow: 'visible' }}>
+          <Grid.Col span={{ base: 4 }}>
+            <Box mx="auto">
+              <AspectRatio ratio={1 / 1}>
+                <BackgroundImage src="/img/Startseite/brauner_kreis.svg">
+                  <div style={{ color: '#f0a893', textAlign: 'center' }}>Finden Statt Suchen</div>
+                </BackgroundImage>
+              </AspectRatio>
+            </Box>
+            <Box mx="auto">
+              <AspectRatio ratio={1 / 1}>
+                <BackgroundImage src="/img/Startseite/brauner_kreis.svg">
+                  <div style={{ color: '#f0a893', textAlign: 'center' }}>Kein Risiko</div>
+                </BackgroundImage>
+              </AspectRatio>
+            </Box>
+          </Grid.Col>
+
+          <Grid.Col span={{ base: 4 }} style={{ overflow: 'visible' }}>
+            <div style={{ height: '100%' }}>
+              <Image
+                src="/img/Startseite/Ivymind_Ranke.svg"
+                fit="contain"
+                style={{
+                  transform: 'translateY(-18%) translateX(-5%)',
+                  overflow: 'visible',
+                  height: '150%',
+                  width: '150%',
+                }}
+              />
+            </div>
+          </Grid.Col>
+
+          <Grid.Col span={{ base: 4 }}>
+            <Box mx="auto">
+              <AspectRatio ratio={1 / 1}>
+                <BackgroundImage src="/img/Startseite/brauner_kreis.svg">
+                  <div style={{ color: '#f0a893', textAlign: 'center' }}>
+                    Kostenloses Erstgespräch
+                  </div>
+                </BackgroundImage>
+              </AspectRatio>
+            </Box>
+            <Box mx="auto">
+              <AspectRatio ratio={1 / 1}>
+                <BackgroundImage src="/img/Startseite/brauner_kreis.svg">
+                  <div style={{ color: '#f0a893', textAlign: 'center' }}>
+                    Qualifizierte Anbieter:innen
+                  </div>
+                </BackgroundImage>
+              </AspectRatio>
+            </Box>
+          </Grid.Col>
+        </Grid>
       </Container>
     </div>
   );
