52ce3fa626c534996b106e9b5d188759dce70c59
Maximilian Lucas <dev@maximilian-lucas.de>
2024-01-02 16:50:45 +0100

    try to fix file loading on vercel deployment

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,24 +1,23 @@
 'use server';
 
-import { promises as fs } from 'fs';
-import { readdir } from 'node:fs/promises';
+import * as fs from 'fs';
+// import { readdir } from 'node:fs/promises';
 import path from 'path';
 
-async function logging() {
-  // Get the current working directory
-  console.log('Logging function');
-  const currentDir = process.cwd();
-  console.log('currentDir');
-  console.log(currentDir);
-  // Read all contents of the current directory
-  const files = await readdir(currentDir);
-  console.log(files);
-}
+// async function logging() {
+//   // Get the current working directory
+//   console.log('Logging function');
+//   const currentDir = process.cwd();
+//   console.log('currentDir');
+//   console.log(currentDir);
+//   // Read all contents of the current directory
+//   const files = await readdir(currentDir);
+//   console.log(files);
+// }
 
 export async function getPostData(slug: string) {
-  const fullPath = path.join(process.cwd(), slug);
-  logging();
-  const markdown = await fs.readFile(fullPath, 'utf-8');
+  const fullPath = path.join(process.cwd(), 'app/blog/markdownFiles', `${slug}.md`);
+  const markdown = fs.readFileSync(fullPath, 'utf-8');
   //const markdown = '# Hier ist gerade etwas schief gelaufen\n### Wir arbeiten daran, dass hier alles so funktioniert wie gewünscht.';
   return markdown;
 }
