7d8509c870bab51c4ae9a351fbba75fd135884b9
jolemse <35252911+jolemse@users.noreply.github.com>
2024-01-09 13:37:01 +0100

    Console Log zum Cookies testen eingefügt

--- components/CookieConsentBanner/cookie.js
+++ components/CookieConsentBanner/cookie.js
@@ -1,41 +1,41 @@
 import Script from 'next/script';
 
 export const getCookies = () => {
-    const cookies = document.cookie.split('; ');
-    const cookieMap = {};
+  const cookies = document.cookie.split('; ');
+  const cookieMap = {};
 
-    for (const cookie of cookies) {
-        const [name, value] = cookie.split('=');
-        cookieMap[name] = decodeURIComponent(value);
-    }
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split('=');
+    cookieMap[name] = decodeURIComponent(value);
+  }
 
-    return cookieMap;
-}
+  return cookieMap;
+};
 
 export const setCookies = (name, value, daysToExpire) => {
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + daysToExpire);
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + daysToExpire);
 
-    const cookieValue = encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; path=/';
-    document.cookie = name + '=' + cookieValue;
-    //console.log("entered setCookies: Set Value ", name + '=' + cookieValue);
-
-}
+  const cookieValue =
+    encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; path=/';
+  document.cookie = name + '=' + cookieValue;
+  //console.log("entered setCookies: Set Value ", name + '=' + cookieValue);
+};
 
 export function googleAnalytics() {
-    //console.log("Triggered Google Analytics");
-    return (
-      <div className="container">
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-JZWQETF6NE" />
-        <Script id="google-analytics">
-          {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-        
-                gtag('config', 'G-JZWQETF6NE');
-                `}
-        </Script>
-      </div>
-    );
-  }
+  console.log("Triggered Google Analytics");
+  return (
+    <div className="container">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JZWQETF6NE" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-JZWQETF6NE');
+          `}
+      </Script>
+    </div>
+  );
+}
