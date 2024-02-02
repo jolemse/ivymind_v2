5370ba479471ee6e88ea59b45c6194441bb664db
jolemse <35252911+jolemse@users.noreply.github.com>
2024-02-02 16:23:28 +0100

    Changed Clarity ID

--- app/metrics/MicrosoftClarity.tsx
+++ app/metrics/MicrosoftClarity.tsx
@@ -12,7 +12,7 @@ const MicrosoftClarity = () => (
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY}");
+        })(window, document, "clarity", "script", "kugzt60v4y");
         `,
     }}
   />
